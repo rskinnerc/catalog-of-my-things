f79bbb6dbcb9487a11a0a556829821a785e76165
wandergithub <wandergonzalez0@hotmail.com>
2022-08-01 15:20:42 -0400

    Add #move_to_archive in Item class with unit test

--- item.rb
+++ item.rb
@@ -1,14 +1,22 @@
-class Item
-  attr_reader :date
+require 'date'
 
-  def initialize(date)
-    @date = date
+class Item
+  attr_reader :publish_date, :archived
+
+  def initialize(publish_date)
+    @publish_date = publish_date
+    @id = rand(500)
+    @archived = false
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
   end
 
   private
 
   def can_be_archived?
-    (today.year - date.year) > 10 ? true : false    
+    (Date.today.year - publish_date.year) > 10 ? true : false    
   end
 
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,8 +4,24 @@ require_relative '../item'
 
 describe Item do
   describe '#move_to_archive' do
-    it '' do
+    it "Set the item attr archived to true for items older than 10 years" do
+      date = Date.new(1987-02-02)
 
+      item = Item.new(date)
+
+      expect(item.archived).to be false
+      item.move_to_archive
+      expect(item.archived).to be true
+    end
+
+    it "does 'not' set archived to true for items 'not' older than 10 years" do
+      date = Date.today
+
+      item = Item.new(date)
+
+      expect(item.archived).to be false
+      item.move_to_archive
+      expect(item.archived).to be false
     end
   end
 end
