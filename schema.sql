455e4532443bf5f8ef692d75298947cef36884a8
Ronald <rjskinnerc@unal.edu.co>
2022-08-01 18:52:31 -0500

    Add games table sql schema

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE authors (
   first_name VARCHAR(255),
   last_name VARCHAR(255),
 )
+
+CREATE TABLE games (
+  id INT UNIQUE,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  PRIMARY KEY (id)
+  CONSTRAINT fk_items_id FOREIGN KEY (id) REFERENCES items(id)
+)
