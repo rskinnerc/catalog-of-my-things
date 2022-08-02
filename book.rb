b79357aa654a9c2b39594b1dac5e22203cc0ba6a
HectorTorresE <hectorjte@gmail.com>
2022-08-02 09:08:17 -0600

    Fix linter

--- book.rb
+++ book.rb
@@ -1,9 +1,8 @@
 require_relative './item'
 
 class Book < Item
-
   attr_accessor :publisher, :cover_state
-  
+
   def initialize(publish_date, archived, id, publisher, cover_state)
     super(publish_date, archived, id)
     @publisher = publisher
@@ -11,7 +10,7 @@ class Book < Item
   end
 
   private
-  
+
   def can_be_archived?()
     return true if super == true || @cover_state == 'bad'
 
