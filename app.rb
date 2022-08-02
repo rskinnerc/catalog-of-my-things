fcceecab2fe6eec6685d442cbc7e61b8c59e7ba3
Ronald <rjskinnerc@unal.edu.co>
2022-08-02 12:26:32 -0500

    Add required actions methods on app class
    - Fix linter errors
    - Update options list
    - Refactor main method to receive an options const

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
+require './game'
+
 class App
   def initialize
+    @games = []
+    @authors = []
     puts 'Welcome to the Catalog of my Things App!'
     puts ''
   end
@@ -12,5 +16,36 @@ class App
         send(option[:action]) if option[:id] == choice
       end
     end
+  end
+
+  def list_games
+    if @games.empty?
+      puts 'There are no games.'
+    else
+      @games.each { |game| puts "Game with ID #{game.id}" }
+    end
+    puts ''
+  end
+
+  def list_authors
+    if @authors.empty?
+      puts 'There are no authors.'
+    else
+      @authors.each { |author| puts "Author with ID #{author.id}" }
+    end
+    puts ''
+  end
+
+  def add_game
+    puts 'Please enter the information for the game:'
+    print 'Publish Date (YYYY-MM-DD): '
+    publish_date = gets.chomp
+    print 'Multiplayer (y/n): '
+    multiplayer = gets.chomp.downcase == 'y'
+    print 'Last played at (YYYY-MM-DD): '
+    last_played_at = gets.chomp
+    @games << Game.new(publish_date, last_played_at, multiplayer: multiplayer)
+    puts 'Game added successfully.'
+    puts ''
   end
 end
--- main.rb
+++ main.rb
@@ -1,13 +1,23 @@
 #!/usr/bin/env ruby
 require './app'
 
-def main
-  options = [
-    { id: 4, name: '4 - List of games', action: 'list_games' },
-    { id: 7, name: '7 - List all authors', action: 'list_authors' },
-    { id: 12, name: '12 - Add a game', action: 'add_game' },
-    { id: 13, name: '13 - Exit', action: 'exit_app' }
-  ]
+OPTIONS = [
+  { id: 1, name: '1 - ', action: '' },
+  { id: 2, name: '2 - ', action: '' },
+  { id: 3, name: '3 - ', action: '' },
+  { id: 4, name: '4 - List of games', action: 'list_games' },
+  { id: 5, name: '5 - ', action: '' },
+  { id: 6, name: '6 - ', action: '' },
+  { id: 7, name: '7 - List all authors', action: 'list_authors' },
+  { id: 8, name: '8 - ', action: '' },
+  { id: 9, name: '9 - ', action: '' },
+  { id: 10, name: '10 - ', action: '' },
+  { id: 11, name: '11 - ', action: '' },
+  { id: 12, name: '12 - Add a game', action: 'add_game' },
+  { id: 13, name: '13 - Exit', action: 'exit_app' }
+].freeze
+
+def main(options)
   app = App.new
   exit = 0
   while exit.zero?
@@ -18,5 +28,5 @@ def main
   end
 end
 
-main
+main(OPTIONS)
 puts 'Thank you for using this app!'
