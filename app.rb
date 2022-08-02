fda2cc5470f17763f1d8dd634e10055f61e88d29
Ronald <rjskinnerc@unal.edu.co>
2022-08-02 11:56:02 -0500

    Add basic app class

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 class App
-  attr_reader :status
-
   def initialize
-    @status = 'Created'
+    puts 'Welcome to the Catalog of my Things App!'
+    puts ''
   end
 
-  def exit_app
-    exit
+  def action(choice, options)
+    if choice > options.length || choice.zero?
+      (puts 'Invalid option. Please try again.')
+    else
+      options.each do |option|
+        send(option[:action]) if option[:id] == choice
+      end
+    end
   end
 end
