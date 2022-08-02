eb2dc5202db7f55da83f80050eb72b30a37bba09
Ronald <rjskinnerc@unal.edu.co>
2022-08-02 12:00:34 -0500

    Update main.rb with the required options

--- main.rb
+++ main.rb
@@ -3,8 +3,10 @@ require './app'
 
 def main
   options = [
-    { id: 1, name: '1 - List all books', action: 'list_books' },
-    { id: 7, name: '7 - Exit', action: 'exit_app' }
+    { id: 4, name: '4 - List of games', action: 'list_games' },
+    { id: 7, name: '7 - List all authors', action: 'list_authors' },
+    { id: 12, name: '12 - Add a game', action: 'add_game' },
+    { id: 13, name: '13 - Exit', action: 'exit_app' }
   ]
   app = App.new
   exit = 0
@@ -12,7 +14,7 @@ def main
     puts 'Please choose an option by entering a number:'
     options.each { |option| puts option[:name] }
     choice = gets.chomp.to_i
-    choice == 7 ? exit = 1 : app.action(choice, options)
+    choice == 13 ? exit = 1 : app.action(choice, options)
   end
 end
 
