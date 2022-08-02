56f9c256751576136ce8c60531e09a20c0082e57
HectorTorresE <hectorjte@gmail.com>
2022-08-02 14:04:05 -0600

    Fix merge

--- main.rb
+++ main.rb
@@ -2,19 +2,16 @@
 require './app'
 
 OPTIONS = [
-  { id: 1, name: '1 - add music album', action: 'add_music_album' },
+  { id: 1, name: '1 - List all books', action: 'list_books' },
   { id: 2, name: '2 - list all music albums', action: 'list_all_music_albums' },
-  { id: 3, name: '3 - list all genres', action: 'list_all_genres' },
-  { id: 4, name: '4 - List of games', action: 'list_games' },
-  { id: 5, name: '5 - ', action: '' },
-  { id: 6, name: '6 - ', action: '' },
-  { id: 7, name: '7 - List all authors', action: 'list_authors' },
-  { id: 8, name: '8 - ', action: '' },
-  { id: 9, name: '9 - ', action: '' },
-  { id: 10, name: '10 - ', action: '' },
-  { id: 11, name: '11 - ', action: '' },
-  { id: 12, name: '12 - Add a game', action: 'add_game' },
-  { id: 13, name: '13 - Exit', action: 'exit_app' }
+  { id: 3, name: '3 - List of games', action: 'list_games' },
+  { id: 4, name: '4 - list all genres', action: 'list_all_genres' },
+  { id: 5, name: '5 - List all labels', action: 'list_labels' },
+  { id: 6, name: '6 - List all authors', action: 'list_authors' },
+  { id: 7, name: '7 - Add a book', action: 'add_book' },
+  { id: 8, name: '8 - add music album', action: 'add_music_album' },
+  { id: 9, name: '9 - Add a game', action: 'add_game' },
+  { id: 10, name: '10 - Exit', action: 'exit_app' }
 ].freeze
 
 def main(options)
@@ -24,7 +21,7 @@ def main(options)
     puts 'Please choose an option by entering a number:'
     options.each { |option| puts option[:name] }
     choice = gets.chomp.to_i
-    choice == 13 ? exit = 1 : app.action(choice, options)
+    choice == 10 ? exit = 1 : app.action(choice, options)
   end
 end
 
