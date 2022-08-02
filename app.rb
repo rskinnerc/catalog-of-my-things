39ab5c90a03d7b258b4fc885562eb8ca3906afe9
wandergithub <wandergonzalez0@hotmail.com>
2022-08-02 14:53:15 -0400

    Implement music and genres options in app

--- app.rb
+++ app.rb
@@ -1,8 +1,44 @@
+require 'date'
+require 'objspace'
+
+require_relative './genre'
+require_relative './music_album'
+
 class App
   attr_reader :status
 
   def initialize
     @status = 'Created'
+  end
+
+  def add_music_album
+    print 'Song Genre: '
+    genre_name = gets.chomp
+    print 'Is this song available in Spotify [y/n]: '
+    on_spotify = gets.chomp.downcase
+
+    music = MusicAlbum.new(Date.today, on_spotify == 'y' ? true : false)
+    genre = Genre.new(genre_name)
+
+    music.add_genre(genre)
+  end
+
+  def list_all_genres
+    ObjectSpace.each_object(Genre) { |genre| puts "#{genre.name}" }
+  end
+
+  def list_all_music_albums
+    ObjectSpace.each_object(MusicAlbum) { |music_album| puts "#{music_album}" }
+  end
+
+  def action(choice, options)
+    if choice > options.length || choice.zero?
+      (puts 'Invalid option. Please try again.')
+    else
+      options.each do |option|
+        send(option[:action]) if option[:id] == choice
+      end
+    end
   end
 
   def exit_app
--- main.rb
+++ main.rb
@@ -4,6 +4,9 @@ require './app'
 def main
   options = [
     { id: 1, name: '1 - List all books', action: 'list_books' },
+    { id: 2, name: '2 - List all music albums', action: 'list_all_music_albums' },
+    { id: 3, name: '3 - List all genres', action: 'list_all_genres' },
+    { id: 4, name: '4 - Add a music album', action: 'add_music_album' },
     { id: 7, name: '7 - Exit', action: 'exit_app' }
   ]
   app = App.new
