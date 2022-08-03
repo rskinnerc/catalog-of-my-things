a7b7269b5bd6fb56cdbdb16cd0e2ec31b3adef9e
wandergithub <wandergonzalez0@hotmail.com>
2022-08-02 22:13:01 -0400

    Build schema.sql music_albums and genres tables

--- schema.sql
+++ schema.sql
@@ -25,3 +25,26 @@ CREATE TABLE labels (
 	title	varchar(50),
 	color	varchar(50)
 )
+
+-- Music_albums and genres
+
+CREATE TABLE music_albums(
+  id INT PRIMARY KEY,
+  on_spotify BOOLEAN,
+  publish_date DATE,
+  archived BOOLEAN,
+  label_id INT,
+  source_id INT,
+  author_id INT,
+  genre_id INT,
+  CONSTRAINT fk_label_id FOREIGN KEY (label_id) REFERENCES label(id)
+  CONSTRAINT fk_source_id FOREIGN KEY (source_id) REFERENCES source(id)
+  CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES author(id)
+  CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) REFERENCES genre(id)
+)
+
+CREATE TABLE genres(
+  id INT PRIMARY KEY,
+  name varchar(50),
+  CONSTRAINT fk_item_id FOREIGN KEY (id) REFERENCES music_albums(id)
+)
