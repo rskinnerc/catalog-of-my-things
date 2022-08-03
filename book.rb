41587b41ffd4ddd6aa54611d28ff70e1e5a266c2
HectorTorresE <hectorjte@gmail.com>
2022-08-03 14:22:01 -0600

    Add save for books

--- book.rb
+++ book.rb
@@ -1,4 +1,5 @@
 require_relative './item'
+require 'json'
 
 class Book < Item
   attr_accessor :publisher, :cover_state
@@ -7,6 +8,24 @@ class Book < Item
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'a' => [id, publish_date, publisher, cover_state, archived, genre&.id, label&.id, author&.id]
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    id, publish_date, publisher, cover_state, archived, genre_id, label_id, author_id = object['a']
+    book = new(publish_date, publisher, cover_state)
+    book.id = id
+    book.archived = archived
+    book.genre = genre_id
+    book.label = label_id
+    book.author = author_id
+    book
   end
 
   private
