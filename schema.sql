0a687457bb79384d316c3bcea569ce5ce45fd288
HectorTorresE <hectorjte@gmail.com>
2022-08-04 09:19:51 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,14 +2,6 @@ CREATE TABLE authors (
   id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
   first_name VARCHAR(255),
   last_name VARCHAR(255)
-  label_id INT,
-  source_id INT,
-  author_id INT,
-  genre_id INT,
-  CONSTRAINT fk_games_label_id FOREIGN KEY (label_id) REFERENCES label(id),
-  CONSTRAINT fk_games_source_id FOREIGN KEY (source_id) REFERENCES source(id),
-  CONSTRAINT fk_games_author_id FOREIGN KEY (author_id) REFERENCES author(id),
-  CONSTRAINT fk_games_genre_id FOREIGN KEY (genre_id) REFERENCES genre(id)
 );
 
 CREATE TABLE games (
@@ -29,11 +21,19 @@ CREATE TABLE games (
 );
 
 CREATE TABLE books (
-	id INT UNIQUE,
+	id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
 	publisher	varchar(50), 
 	cover_state	varchar(50), 
-	PRIMARY KEY(id),
-  CONSTRAINT fk_items_id FOREIGN KEY (id) REFERENCES items(id)
+  publish_date DATE,
+  archived BOOLEAN,
+  label_id INT,
+  source_id INT,
+  author_id INT,
+  genre_id INT,
+  CONSTRAINT fk_games_label_id FOREIGN KEY (label_id) REFERENCES label(id),
+  CONSTRAINT fk_games_source_id FOREIGN KEY (source_id) REFERENCES source(id),
+  CONSTRAINT fk_games_author_id FOREIGN KEY (author_id) REFERENCES author(id),
+  CONSTRAINT fk_games_genre_id FOREIGN KEY (genre_id) REFERENCES genre(id)
 )
 
 CREATE TABLE labels (
